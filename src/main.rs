b3fce270c164af0b0fd94cb9d3f6628ff4db6e9e
wiwi <kohnnoam@gmail.com>
2025-04-22 00:10:32 +0200

    finish soon

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,6 @@ use train::*;
 mod blackjack;
 use blackjack::*;
 fn main(){
-    let mut game = Game::new(10);
+    train_ia();
     
-    game.deal_to_dealer(2);
-//    game.deal_to_player(2, 0);
-    game.deal_choosen_card(vec![Card::Jack, Card::Jack], 0);
-    let bm = best_mooves(&game, 0);
-    println!("game: {:?}", bm);
-    game.play(&bm[0], false, 0);
-    game.dealer_turn();
-    game.show();
-    println!("result: {}", game.result());
 }
